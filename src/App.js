ee610707cd70cf50b00e85ee38cdc251020ced2c
tsishchanka <e.tsish@gmail.com>
2022-07-15 16:39:16 +0300

    bugfix/toast add

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
 import './App.css';
-import { toastToShow, ToastRefContainer } from '@tsishchanka/edu/dist';
+import { ToastRefContainer } from '@tsishchanka/edu/dist';
 
 function App() {
-  const handleClick = () => {
-    toastToShow.addToast({
-      type: 'error',
-      title: 'Error',
-      position: 'top-left',
-      autoDelete: 'false',
-      deleteDelay: '2000',
-      bgColor: 'red',
-    });
-  };
-
   return (
     <div className='App'>
       <header className='App-header'>
-        <button onClick={handleClick}>SHOW</button>
-        <ToastRefContainer
-          type='warning'
-          title='Info'
-          position='top-left'
-          autoDelete={false}
-          deleteDelay={2000}
-          bgColor='#F4E048'
-          description='Info description'
-          icon='info'
-        />
+        <ToastRefContainer />
       </header>
     </div>
   );
